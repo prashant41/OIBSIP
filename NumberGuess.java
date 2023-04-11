8715564562a8c4601e0d0d54b39ce41f1e864fb8
Prashant Poojary <poojaryprashant35@gmail.com>
2023-04-11 19:34:08 +0530

    Update NumberGuess.java

--- NumberGuess.java
+++ NumberGuess.java
@@ -1,43 +1,99 @@
-import java.util.Scanner;
+package application;
+	
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NumberGuess {
-    public static void main(String[] args) {
-        int secretNumber = (int)(Math.random() * 100) + 1;
-        int guess;
-        int numGuesses = 0;
-        int playerScore = 100;
-        boolean isWin = false;
-        Scanner scanner = new Scanner(System.in);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-        System.out.println("Welcome to the Number Guessing Game!");
+public class Main extends Application {
 
-        while (!isWin) {
-            System.out.print("Enter your guess (1-100): ");
-            guess = scanner.nextInt();
-            numGuesses++;
+    private int randomNumber;
+    private int numGuesses = 0;
+    private int maxGuesses = 10;
+    private int score = 0;
+    private List<Integer> guessHistory = new ArrayList<>();
 
-            if (guess == secretNumber) {
-                isWin = true;
-                System.out.println("You win! The number was " + secretNumber);
-                System.out.println("It took you " + numGuesses + " guesses.");
-                System.out.println("Your score is " + playerScore);
-            } else if (guess < secretNumber) {
-                System.out.println("Too low! Guess again.");
-                playerScore = playerScore -10;
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Guess a number between 1 and 100:");
+        TextField textField = new TextField();
+        Button submitButton = new Button("Submit");
+        Button hintButton = new Button("Hint");
+        Button historyButton = new Button("History");
+        Label scoreLabel = new Label("Score: 0");
+
+        submitButton.setOnAction(event -> {
+            int guess = Integer.parseInt(textField.getText());
+            guessHistory.add(guess);
+            if (guess == randomNumber) {
+                label.setText("You guessed the number in " + numGuesses + " tries!\n" + "The number was " + randomNumber + "\n" + "Thanks for playing!");
+                textField.setDisable(true);
+                submitButton.setDisable(true);
+                hintButton.setDisable(true);
+                score++;
+                scoreLabel.setText("Score: " + score);
+            } else if (guess < randomNumber) {
+                label.setText("Too low! Guess again:");
+                numGuesses++;
+                if (numGuesses >= maxGuesses) {
+                    label.setText("You have reached the maximum number of guesses.\n" + "The number was " + randomNumber + "\n" + "Thanks for playing!");
+                    textField.setDisable(true);
+                    submitButton.setDisable(true);
+                    hintButton.setDisable(true);
+                }
             } else {
-                System.out.println("Too high! Guess again.");
-                playerScore = playerScore - 10;
-            }
-            //Hint option
-            System.out.println("Do you want a hint? y/n");
-            String hintOption = scanner.next();
-            if (hintOption.equals("y")) {
-                if (guess < secretNumber) {
-                    System.out.println("Hint: The number is higher than " + guess);
-                } else {
-                    System.out.println("Hint: The number is lower than " + guess);
+                label.setText("Too high! Guess again:");
+                numGuesses++;
+                if (numGuesses >= maxGuesses) {
+                    label.setText("You have reached the maximum number of guesses.\n" + "The number was " + randomNumber + "\n" + "Thanks for playing!");
+                    textField.setDisable(true);
+                    submitButton.setDisable(true);
+                    hintButton.setDisable(true);
                 }
             }
-        }
+        });
+
+        hintButton.setOnAction(event -> {
+            if (randomNumber % 2 == 0) {
+                label.setText("Hint: The number is even.");
+            } else {
+                label.setText("Hint: The number is odd.");
+            }
+        });
+
+        historyButton.setOnAction(event -> {
+            String history = "";
+            for (int i = 0; i < guessHistory.size(); i++) {
+                history += "Guess #" + (i + 1) + ": " + guessHistory.get(i) + "\n";
+            }
+            label.setText("Guess history:\n" + history);
+        });
+
+        HBox hBox = new HBox(20, submitButton, hintButton, historyButton, scoreLabel);
+        hBox.setAlignment(Pos.CENTER);
+        VBox vBox = new VBox(20, label, textField, hBox);
+        vBox.setPadding(new Insets(20));
+        vBox.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(vBox);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Number Guessing Game");
+        primaryStage.show();
+        randomNumber = new Random().nextInt(100) + 1;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
